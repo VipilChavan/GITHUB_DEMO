d1cc132bbc0533008ec898ae417621f80905b2c6
VipilChavan <chavan.vipil@gmail.com>
2023-06-21 17:08:49 +0530

    First Commit.Committed DEV_DEMO and TEST_DEMO.

---
+++ src/main/java/DEV_DEMO.java
@@ -0,0 +1,6 @@
+public class DEV_DEMO {
+
+    public static void main(String[] args) {
+        System.out.println("I am from main DEV_DEMO CLASS");
+    }
+}
---
+++ src/test/java/TEST_DEMO.java
@@ -0,0 +1,6 @@
+public class TEST_DEMO {
+
+    public static void main(String[] args) {
+        System.out.println("I am from test TEST_DEMO CLASS");
+    }
+}
