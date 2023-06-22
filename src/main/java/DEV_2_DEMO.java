5bb71151036460f8aa8f3ebfdbdb1bfde7989e09
VipilChavan <113865455+VipilChavan@users.noreply.github.com>
2023-06-22 09:22:43 +0530

    Update DEV_2_DEMO.java

--- src/main/java/DEV_2_DEMO.java
+++ src/main/java/DEV_2_DEMO.java
@@ -1,2 +1,10 @@
-       // System.out.println("I am from main DEV_DEMO CLASS");
+public class DEV_2_DEMO {
+
+    public static void main(String[] args) {
+        System.out.println("I am from main DEV_2_DEMO CLASS");
+        System.out.println("Added from git_hub");
+        //adding new line
+        System.out.println("Added from ide to git_hub");
+    }
+}       // System.out.println("I am from main DEV_DEMO CLASS");
         
